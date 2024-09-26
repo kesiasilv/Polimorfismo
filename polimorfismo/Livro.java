29a9274b084af14c063ca5c28c888724bea579a8
Késia Silva <kesiasilvab6@gmail.com>
2024-09-26 17:37:29 -0300

    Add files via upload

---
+++ polimorfismo/Livro.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.polimorfismo;
+
+/**
+ *
+ * @author kesia.viana
+ */
+public class Livro {
+    private String titulo;
+    private String autor;
+    private double preco;
+
+    public Livro(String titulo, String autor, double preco) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.preco = preco;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Preço: R$ " + preco);
+    }
+}
+
+    
+
